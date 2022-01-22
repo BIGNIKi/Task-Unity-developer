fab3ae8e93d60ea7cc98ff53dd3de11095baa3b9
BIGNIKi <nikita_sr1@mail.ru>
2022-01-22 15:10:01 +0700

    Close/open

---
+++ Task-Unity-developer/Assets/Scripts/DarkLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkLogic : MonoBehaviour
+{
+    public CanvasGroup canvas;
+
+    private int isShouldOpen = 1; // 1 - will open, -1 - will close
+    private int numProcesses = 0;
+
+    public void OnClickCallback()
+    {
+        isShouldOpen = isShouldOpen == -1 ? 1:-1 ;
+        if(numProcesses == 0)
+        {
+            numProcesses++;
+            StartCoroutine(OpeningCloseDark());
+        }
+    }
+
+    private IEnumerator OpeningCloseDark()
+    {
+        float time = canvas.alpha;
+        do
+        {
+            time += Time.deltaTime * isShouldOpen;
+            canvas.alpha = time;
+            yield return null;
+        } while(canvas.alpha > 0 && canvas.alpha < 1);
+        numProcesses--;
+        yield break;
+    }
+}
